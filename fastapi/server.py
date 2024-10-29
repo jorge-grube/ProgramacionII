f80ff70d302c6cd67a6d62fcc8082f2f78476bab
Jaime <joriolgo@gmail.com>
2024-10-29 15:29:23 -0400

    Correccion de server.py y requirements.txt para pull request

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,19 +1,21 @@
-from fastapi import FastAPI
-from clientes.endpoints.endpoints import router as clientes_router
-
-app = FastAPI()
-
-app.include_router(clientes_router, prefix="/api")
-
-import shutil
-import io
-import pandas as pd
-from typing import List
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, HTTPException, Depends, File, UploadFile, Form
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel as PydanticBaseModel
+from typing import List
+import pandas as pd
+from clientes.endpoints.endpoints import router as clientes_router
 
-# Clases existentes de ejemplo para la funcionalidad de contratos
+# Instancia principal de la aplicación FastAPI
+app = FastAPI(
+    title="Gestión de Clínica Veterinaria",
+    description="API para la gestión de datos de la clínica veterinaria y otras funcionalidades.",
+    version="0.2.0",
+)
+
+# Incluye el router para los clientes
+app.include_router(clientes_router, prefix="/api")
+
+# Clases y funcionalidades adicionales (Contratos, Formularios)
 class BaseModel(PydanticBaseModel):
     class Config:
         arbitrary_types_allowed = True
@@ -39,14 +41,7 @@ class Contrato(BaseModel):
 class ListadoContratos(BaseModel):
     contratos = List[Contrato]
 
-# Instancia principal de la aplicación FastAPI
-app = FastAPI(
-    title="Gestión de Clínica Veterinaria",
-    description="""API para la gestión de datos de la clínica veterinaria y otras funcionalidades.""",
-    version="0.2.0",
-)
-
-# Endpoint para recuperar datos de contratos (funcionalidad existente)
+# Endpoint para recuperar datos de contratos
 @app.get("/retrieve_data/")
 def retrieve_data():
     todosmisdatos = pd.read_csv('./contratos_inscritos_simplificado_2023.csv', sep=';')
@@ -56,7 +51,7 @@ def retrieve_data():
     listado.contratos = todosmisdatosdict
     return listado
 
-# Endpoint para envío de formularios (funcionalidad existente)
+# Endpoint para envío de formularios
 class FormData(BaseModel):
     date: str
     description: str
